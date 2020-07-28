bb84263d8e4d57d1175ba363cf338a3edda2c594
sofm <yasuo@lol.com>
2020-07-28 10:44:42 +0800

    first commit

---
+++ opt.py
@@ -0,0 +1,16 @@
+#opt参数
+
+ngf = 96
+ndf = 96
+nz = 100
+img_size = 96
+batch_size = 100
+num_workers = 4
+lr1 = 0.0002
+lr2 = 0.0002
+beta1 =0.5
+epochs = 200
+d_every = 1
+g_every = 5
+save_every = 20
+data_path = r"F:\迅雷下载\11月班\2020-05_15_GAN"
