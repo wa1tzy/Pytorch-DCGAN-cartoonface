707e4e49b92d1b2db14edaa92de8dc0f045db2ed
sofm <yasuo@lol.com>
2020-07-28 10:32:34 +0800

    first commit

---
+++ G_Net.py
@@ -0,0 +1,27 @@
+import opt
+import torch.nn as nn
+
+
+class NetG(nn.Module):
+    def __init__(self):
+        super().__init__()
+        ngf = opt.ngf
+        self.conv_layer = nn.Sequential(
+            nn.ConvTranspose2d(opt.nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=True),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ngf, 3, 5, 3, 1, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        return self.conv_layer(x)
